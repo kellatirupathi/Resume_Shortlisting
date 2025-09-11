f22830c793a167c042c65f8ee76fe1513efa9dd2
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-09-11 10:59:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3007,7 +3007,7 @@ def analyze_text_with_gemini(prompt):
         if not current_api_key:
             continue
 
-        gemini_api_url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={current_api_key}"
+        gemini_api_url = f"https://generativelanguage.googleapis.com/v1/models/gemma-3-27b-it:generateContent?key={current_api_key}"
 
         # API request payload
         payload = {
